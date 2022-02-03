f906c661ca7c8ce43e1d7b22d409752ee44e57a9
alexprystupa <78055891+alexprystupa@users.noreply.github.com>
2022-02-03 08:55:20 -0500

    Updated Wordle

--- testing_word_storage.py
+++ testing_word_storage.py
@@ -1,27 +1,69 @@
 from termcolor import colored
 
-word = 'moist'
-guess = 'toase'
+#word = 'fail'
+#guess = 'fale'
 
-storage_dict = dict()
+'''
+if index and letter match append to correct list.
+else if index does not match but the letter appears in the word append to incorrect list
+else append to not in word list
+'''
 
-list_of_word_letters = []
-for word_letter in enumerate(word):
-    list_of_word_letters.append(word_letter)
+#correct_letter = []
+#incorrect_letter = []
+#not_in_word = []
+#for i, letter in enumerate(guess):
+    #if guess[i] == word[i]:
+        #correct_letter.append(letter)
+    #elif letter in word:
+        #incorrect_letter.append(letter)
+    #else:
+        #not_in_word.append(letter)
 
-list_of_guess_letters = []
-for guess_letter in enumerate(guess):
-    list_of_guess_letters.append(guess_letter)
+#print(f'Correct Letter: {correct_letter}')
+#print(f'Incorrect Letter: {incorrect_letter}')
+#print(f'Not in word: {not_in_word}')
 
-print(list_of_word_letters)
-print(list_of_guess_letters)
+# THIS VERSION FINALLY WORKS!
+#colored_guess = []
+#for i, letter in enumerate(guess):
+    #if letter in correct_letter and guess[i] == word[i]:
+        #colored_guess.append(colored(letter, 'green'))
+    #elif letter in incorrect_letter and letter in correct_letter:
+        #colored_guess.append(colored(letter, 'red'))
+    #elif letter in incorrect_letter:
+        #colored_guess.append(colored(letter, 'yellow'))
+    #else:
+       # colored_guess.append(colored(letter, 'red'))
 
-#for word_tup in list_of_word_letters:
-    #for guess_tup in list_of_guess_letters:
-        #if word_tup == guess_tup:
-            #print('Success')
+#print(*colored_guess)
 
-for i, word_letter in word:
-    for j, guess_letter in guess:
-        if word_letter[i] == guess_letter[j]:
-            print('Success')
+def return_colored_word(word, guess):
+    correct_letter = []
+    incorrect_letter = []
+    not_in_word = []
+
+    for i, letter in enumerate(guess):
+        if guess[i] == word[i]:
+            correct_letter.append(letter)
+        elif letter in word:
+            incorrect_letter.append(letter)
+        else:
+            not_in_word.append(letter)
+
+    # THIS VERSION FINALLY WORKS!
+    colored_guess = []
+    for i, letter in enumerate(guess):
+        if letter in correct_letter and guess[i] == word[i]:
+            colored_guess.append(colored(letter, 'green'))
+        elif letter in incorrect_letter and letter in correct_letter:
+            colored_guess.append(colored(letter, 'red'))
+        elif letter in incorrect_letter:
+            colored_guess.append(colored(letter, 'yellow'))
+        else:
+            colored_guess.append(colored(letter, 'red'))
+
+    print(*colored_guess)
+
+if __name__ == '__main__':
+    return_colored_word('knoll', 'knoll')
--- wordle.py
+++ wordle.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
 # wordle.py
+'''
+This script was made to mimic the popular worlde game
+that was just aquired by the NY Times.
+'''
 import sys
-
-from termcolor import colored
 import os
+from termcolor import colored
 
 def clear_board():
+    '''Function to clear board'''
     os.system('clear')
 
 def make_wordle_word():
+    '''Let the user define a worlde to guess'''
+
     wordle_word = input('Wordle of the day: ')
     while len(wordle_word) != 5:
         print('Please input a 5 letter word')
@@ -16,11 +22,25 @@ def make_wordle_word():
 
     return wordle_word
 
+def wordle_check_word(wordle_word):
+    '''
+    Takes in user defined worlde word and outputs the colorized
+    green version so we can check player guess against it
+    '''
+    green_wordle = []
+
+    for letter in wordle_word:
+        green_wordle.append(colored(letter, 'green'))
+
+    return green_wordle
+
 def print_board(rows):
+    '''Prints the board'''
     for row in rows:
         print(*row)
 
 def take_guess():
+    '''Takes in the user guess'''
     list_of_guess = []
     guess = input('Take a guess: ')
     for letter in guess:
@@ -28,7 +48,41 @@ def take_guess():
 
     return list_of_guess
 
+def return_colored_word(word, guess):
+    '''
+    Takes in the worlde word and user guess to return
+    the colorized version of it to be printed
+    '''
+    correct_letter = []
+    incorrect_letter = []
+    not_in_word = []
+
+    for i, letter in enumerate(guess):
+        if guess[i] == word[i]:
+            correct_letter.append(letter)
+        elif letter in word:
+            incorrect_letter.append(letter)
+        else:
+            not_in_word.append(letter)
+
+    # THIS VERSION FINALLY WORKS!
+    colored_guess = []
+    for i, letter in enumerate(guess):
+        if letter in correct_letter and guess[i] == word[i]:
+            colored_guess.append(colored(letter, 'green'))
+        elif letter in incorrect_letter and letter in correct_letter:
+            colored_guess.append(colored(letter, 'red'))
+        elif letter in incorrect_letter:
+            colored_guess.append(colored(letter, 'yellow'))
+        else:
+            colored_guess.append(colored(letter, 'red'))
+
+    return colored_guess
+
 def main():
+    '''
+    Making the game play for wordle
+    '''
 
     # Making beginning board
     row1 = ['_'] * 5
@@ -42,28 +96,25 @@ def main():
     wordle_word = make_wordle_word()
     print_board(wordle_rows)
 
-    for i in range(0,6):
-        wordle_rows[i] = take_guess()
+    for i in range(0, 6):
+        wordle_rows[i] = return_colored_word(wordle_word, take_guess())
         clear_board()
         print_board(wordle_rows)
 
         for word in wordle_rows:
-            if ''.join(word) == wordle_word:
+            if word == wordle_check_word(wordle_word):
+            #if ''.join(word) == wordle_word:
                 print('Congrats, you have solved the wordle!')
                 sys.exit(0)
+
+    if wordle_rows[5] != row6 and wordle_rows[5] != wordle_check_word(wordle_word):
+        print('Sorry the correct word was: ')
+        print(*wordle_check_word(wordle_word))
+
+            #if wordle_rows[5] != wordle_check_word(wordle_word):
+                #print('Sorry!')
 
 
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
-
-
-
-
-
